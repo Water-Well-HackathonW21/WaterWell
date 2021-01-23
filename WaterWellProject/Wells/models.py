34d36bc2494b07c8a963e0a11e1d1c1f608763ba
Maaz Siddique <siddiqma98@gmail.com>
2021-01-23 16:49:00 -0700

    Enter input for marker

--- WaterWellProject/Wells/models.py
+++ WaterWellProject/Wells/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# class Subscribe(models.Model):
+#     email = models.EmailField(max_length = 200, null = False, blank = False)
+#     country = models.CharField(max_length = 200, null = True, blank = True)
+#     city = models.CharField(max_length = 200, null = True, blank = True)
+#     store = models.CharField(max_length = 200, null = True, blank = True)
+#     active = models.BooleanField(default=True)
+
+#     class Meta: 
+#         db_table = "subscribedb"
