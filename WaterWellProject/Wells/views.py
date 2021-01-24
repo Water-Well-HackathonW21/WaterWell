e22173af891f7e9a8bf3e799330f0fe64b09f07f
Max Melendez <mmelende@ualberta.ca>
2021-01-24 10:50:58 -0700

    Added backend functionality

--- WaterWellProject/Wells/views.py
+++ WaterWellProject/Wells/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Well
 import json
 from django.core import serializers
+from .forms import WellMarker
 
 # Create your views here.
 
@@ -14,3 +15,25 @@ def index (request):
     json_serializer = serializers.get_serializer("json")()
     all_items = json_serializer.serialize(Well.objects.all())
     return render (request, 'Wells/main.html', {'all_items': all_items})
+
+def add (request):
+    if request.method == 'POST':
+        print(request.body)
+        res = json.loads(request.body)
+        print(res["status"])
+        Well(status = res["status"], lat = res["lat"], lon = res["lon"], description = res["description"]).save()
+        # WellMarker.objects.create(status = res["status"], lat = res["lat"], lon = res["lon"], description = res["description"])
+        json_serializer = serializers.get_serializer("json")()
+        all_items = json_serializer.serialize(Well.objects.all())
+
+
+
+        return render (request, 'Wells/main.html', {'all_items': all_items})
+    #     form = WellMarker(request.POST or None)
+    #     print(request.body)
+
+    # if form.is_valid():
+    #     form.save()
+    #     json_serializer = serializers.get_serializer("json")()
+    #     all_items = json_serializer.serialize(Well.objects.all())
+        
